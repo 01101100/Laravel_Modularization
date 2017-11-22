5bf290c70824333866df2323549112d839c27f92
01101100 <long.dragon96@gmail.com>
2017-11-22 23:19:06 +0700

    add method to ProductRepositoryInterface

--- app/Repositories/Interfaces/ProductRepositoryInterface.php
+++ app/Repositories/Interfaces/ProductRepositoryInterface.php
@@ -4,11 +4,26 @@
  * @Author: 01101100
  * @Date:   2017-11-04 19:24:53
  * @Last Modified by:   01101100
- * @Last Modified time: 2017-11-04 19:26:13
+ * @Last Modified time: 2017-11-22 23:08:43
  */
 
 namespace App\Repositories\Interfaces;
 
 interface ProductRepositoryInterface {
 	// interface will be written here
+	/**
+	 * [findByAttr description]
+	 * @param  [type] $field     [description]
+	 * @param  string $operation [description]
+	 * @param  string $value     [description]
+	 * @return [type]            [description]
+	 */
+	public function findByAttr($field, $operation = '=', $value = '');
+
+	/**
+	 * [search description]
+	 * @param  string $keyword [description]
+	 * @return [type]          [description]
+	 */
+	public function search($keyword = '');
 }
