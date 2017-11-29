b46b197817c435f037fac79a55a333538d9e0b13
NeverSayNeverLove <NeverSayNeverLoveMD@gmail.com>
2017-11-29 17:10:03 +0700

    change database

--- config/constants.php
+++ config/constants.php
@@ -1,10 +1,11 @@
 <?php
 
-define('UNKNOW', -1);
-define('PENDING', 0);
-define('PROCESSING', 1);
-define('SHIPPING', 2);
-define('RECEIVED', 3);
-define('FAILED', 4);
+if (!defined('UNKNOW')) define('UNKNOW', -1);
+if (!defined('PENDING')) define('PENDING', 0);
+if (!defined('PROCESSING')) define('PROCESSING', 1);
+if (!defined('SHIPPING')) define('SHIPPING', 2);
+if (!defined('RECEIVED')) define('RECEIVED', 3);
+if (!defined('FAILED')) define('FAILED', 4);
+
 
 ?>
